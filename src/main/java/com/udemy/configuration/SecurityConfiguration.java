8b407b83e78b12ed0a8d9d7f18c0d58280d96a16
Jorge <georgevazj@gmail.com>
2017-01-05 13:06:47 +0100

    Configuracion de Spring Security

---
+++ src/main/java/com/udemy/configuration/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.udemy.configuration;
+
+import com.udemy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by jorge on 5/01/17.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("userService")
+    private UserDetailsService userService;
+
+    //Servira para añadir el UserDetailService
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/css/*", "/imgs/*").permitAll() //Permite cargar los recursos estaticos
+            .anyRequest().authenticated()
+            .and()
+        .formLogin().loginPage("/login").loginProcessingUrl("/logincheck")
+        .usernameParameter("username").passwordParameter("password")
+        .defaultSuccessUrl("/loginsuccess").permitAll()
+        .and()
+        .logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll();
+    }
+}
