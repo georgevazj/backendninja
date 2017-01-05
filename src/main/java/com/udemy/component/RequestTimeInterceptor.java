c7d9ccc2bf431cf024879f5fc2973677a2b120f2
Jorge <georgevazj@gmail.com>
2017-01-05 14:00:03 +0100

    Creada auditoria de aplicacion

--- src/main/java/com/udemy/component/RequestTimeInterceptor.java
+++ src/main/java/com/udemy/component/RequestTimeInterceptor.java
@@ -1,12 +1,18 @@
 package com.udemy.component;
 
+import com.udemy.repository.LogRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  * Created by Jorge on 2/1/17.
@@ -14,6 +20,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    @Qualifier("logRepository")
+    private LogRepository logRepository;
 
     private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
 
@@ -28,6 +38,15 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         long startTime = (long) request.getAttribute("startTime");
-        LOG.info("Url to: '" + request.getRequestURL().toString() + "' in: '" + (System.currentTimeMillis() - startTime) + "'ms'");
+        String url = request.getRequestURL().toString();
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = "";
+        if(auth!=null && auth.isAuthenticated()){
+            username = auth.getName();
+        }
+        com.udemy.entity.Log log = new com.udemy.entity.Log(new Date(), auth.getDetails().toString(), username, url);
+        logRepository.save(log);
+        LOG.info("Url to: '" + url + "' in: '" + (System.currentTimeMillis() - startTime) + "'ms'");
     }
 }
