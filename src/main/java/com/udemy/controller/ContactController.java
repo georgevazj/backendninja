712f7b5895f0ed5f5c0fbc50636b66d138daaead
Jorge <georgevazj@gmail.com>
2017-01-05 13:40:09 +0100

    Demo para securizar los controladores

--- src/main/java/com/udemy/controller/ContactController.java
+++ src/main/java/com/udemy/controller/ContactController.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +39,7 @@ public class ContactController {
         return mav;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @GetMapping("/contactform")
     public String redirectContactForm(
             @RequestParam(name="id", required = false) int id,
